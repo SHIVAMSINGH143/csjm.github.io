ab0ea492bc4a15968a973addb2577cf6ddbc70cd
SHIVAMSINGH143 <70458110+SHIVAMSINGH143@users.noreply.github.com>
2020-08-31 08:55:49 +0530

    csjmtrip

---
+++ index.js
@@ -0,0 +1,7 @@
+function confirmation() {
+    confirm("PLEASE CHECK YOUR FORM CAREFULLY BEFORE FORM SUBMISSION");
+}
+function alert1() {
+    alert("PLEASE FILL FORM IN CAPITAL LETTERS.")
+    
+}
